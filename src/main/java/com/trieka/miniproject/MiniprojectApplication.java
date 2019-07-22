e5ed927e239058b79684573bc1980b894fb2159b
putratrieka <putratrieka@gmail.com>
2019-07-22 22:18:09 +0700

    camunda config

--- src/main/java/com/trieka/miniproject/MiniprojectApplication.java
+++ src/main/java/com/trieka/miniproject/MiniprojectApplication.java
@@ -1,15 +1,25 @@
 package com.trieka.miniproject;
 
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableProcessApplication
 @SpringBootApplication
 public class MiniprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MiniprojectApplication.class, args);
 	}
+	@Autowired
+	private RuntimeService runtimeService;
 	
+	private void processPostDeploy(PostDeployEvent event) {
+		runtimeService.startProcessInstanceByKey("payment");
+	}
 	
 
 }
---
+++ src/main/java/com/trieka/miniproject/service/CutiService.java
@@ -0,0 +1,5 @@
+package com.trieka.miniproject.service;
+
+public class CutiService {
+
+}
